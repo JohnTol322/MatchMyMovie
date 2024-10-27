98c140c09434c8ea3f42690b4d614a76c4f95830
John Tol <johntol322@gmail.com>
2024-10-27 16:16:07 +0100

    #3 - Replaced ApiConfig to seperate config package

--- api/src/main/java/com/MatchMyMovie/api/config/ApiConfig.java
+++ api/src/main/java/com/MatchMyMovie/api/config/ApiConfig.java
@@ -1,4 +1,4 @@
-package com.MatchMyMovie.api;
+package com.MatchMyMovie.api.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
