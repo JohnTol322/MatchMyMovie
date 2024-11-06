78e2ce032bdc6e152a7867d3c732de93a7b16a4b
John Tol <johntol322@gmail.com>
2024-11-06 23:03:54 +0100

    #4 - Added swipe animation + posters are a stack of 3 now instead of 1 single poster

--- web/src/components/matcher/SwipePage.tsx
+++ web/src/components/matcher/SwipePage.tsx
@@ -7,6 +7,8 @@ const SwipePage = () => {
 
     const [movies, setMovies] = React.useState<Movie[]>([]);
     const [currentIndex, setCurrentIndex] = React.useState(0);
+    const [swipeRight, setSwipeRight] = React.useState(false);
+    const [swipeLeft, setSwipeLeft] = React.useState(false);
 
     useEffect(() => {
 
@@ -20,15 +22,60 @@ const SwipePage = () => {
 
     }, [setMovies]);
 
+    const currentStack = [
+        movies[currentIndex],
+        movies[currentIndex + 1],
+        movies[currentIndex + 2]
+    ];
+
+    const handleSwipeRight = () => {
+        if (swipeRight || swipeLeft) return;
+
+        setSwipeRight(true);
+        setTimeout(() => {
+            setCurrentIndex(currentIndex + 1);
+            setSwipeRight(false);
+        }, 500);
+    };
+
+    const handleSwipeLeft = () => {
+        if (swipeRight || swipeLeft) return;
+
+        setSwipeLeft(true);
+        setTimeout(() => {
+            setCurrentIndex(currentIndex + 1);
+            setSwipeLeft(false);
+        }, 500);
+    }
+
+    const swipeClass: string = swipeRight ? "likeSwipe" : swipeLeft ? "dislikeSwipe" : "";
+
     return (
         <div className="swipe-page">
             <div className="swipe-box">
                 {
-                    movies[currentIndex] === undefined ? <div>Loading...</div> : <img className="movie-poster" src={`https://image.tmdb.org/t/p/w500/${movies[currentIndex]?.poster_path}`} alt={"profile"}/>
+                    movies[currentIndex] === undefined ?
+                        <div>Loading...</div> :
+                        <div className="movie-stack">
+                            {
+                                currentStack.map((movie, index) => (
+                                    <img className={`movie-poster ${index === 0 ? swipeClass : ""}`}
+                                         style={{
+                                             top: `${index * 8}px`,
+                                             left: `${index * 8}px`,
+                                             zIndex: currentStack.length - index
+                                         }}
+                                         src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                                    />
+                                ))
+                            }
+                        </div>
                 }
                 <div className="swipe-buttons">
-                    <img onClick={() => setCurrentIndex(currentIndex+1)} src={require("../../assets/images/dislikeButton.png")} alt={"dislike"}/>
-                    <img onClick={() => setCurrentIndex(currentIndex+1)} src={require("../../assets/images/likeButton.png")} alt={"dislike"}/>
+                    <img onClick={handleSwipeLeft}
+                         src={require("../../assets/images/dislikeButton.png")} alt={"dislike"}/>
+                    <img onClick={handleSwipeRight}
+                         src={require("../../assets/images/likeButton.png")} alt={"dislike"}/>
                 </div>
             </div>
         </div>
