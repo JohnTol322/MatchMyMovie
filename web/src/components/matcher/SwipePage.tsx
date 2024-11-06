925873713abfdedadc0ef2d875e048e8a5838734
John Tol <johntol322@gmail.com>
2024-11-06 18:01:39 +0100

    #4 - Loaded movies in to view with posters, clicking like/dislike button will trigger next movie to show

--- web/src/components/matcher/SwipePage.tsx
+++ web/src/components/matcher/SwipePage.tsx
@@ -1,14 +1,34 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "./SwipePage.scss";
+import {movieService} from "../../services/MovieService";
+import {Movie} from "../../models/Movie";
 
 const SwipePage = () => {
+
+    const [movies, setMovies] = React.useState<Movie[]>([]);
+    const [currentIndex, setCurrentIndex] = React.useState(0);
+
+    useEffect(() => {
+
+        if (movies.length > 0) {
+            return;
+        }
+
+        movieService.discoverMovies()
+            .then(setMovies)
+            .catch(console.error);
+
+    }, [setMovies]);
+
     return (
         <div className="swipe-page">
             <div className="swipe-box">
-                <img className="movie-poster" src={"https://via.placeholder.com/300"} alt={"profile"}/>
+                {
+                    movies[currentIndex] === undefined ? <div>Loading...</div> : <img className="movie-poster" src={`https://image.tmdb.org/t/p/w500/${movies[currentIndex]?.poster_path}`} alt={"profile"}/>
+                }
                 <div className="swipe-buttons">
-                    <img src={require("../../assets/images/dislikeButton.png")} alt={"dislike"}/>
-                    <img src={require("../../assets/images/likeButton.png")} alt={"dislike"}/>
+                    <img onClick={() => setCurrentIndex(currentIndex+1)} src={require("../../assets/images/dislikeButton.png")} alt={"dislike"}/>
+                    <img onClick={() => setCurrentIndex(currentIndex+1)} src={require("../../assets/images/likeButton.png")} alt={"dislike"}/>
                 </div>
             </div>
         </div>
