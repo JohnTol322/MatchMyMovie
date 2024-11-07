50f7959567b2852d9f94cbbe370813d16bc71d46
John Tol <johntol322@gmail.com>
2024-11-07 17:11:07 +0100

    #4 - Changed response of movie controller to proper response entity

--- api/src/main/java/com/MatchMyMovie/api/controller/MovieController.java
+++ api/src/main/java/com/MatchMyMovie/api/controller/MovieController.java
@@ -3,6 +3,7 @@ package com.MatchMyMovie.api.controller;
 import com.MatchMyMovie.api.model.ApiResponse;
 import com.MatchMyMovie.api.model.movie.Movie;
 import com.MatchMyMovie.api.service.MovieService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,12 +20,16 @@ public class MovieController {
         this.movieService = movieService;
     }
 
-    @GetMapping()
-    public ApiResponse<List<Movie>> getMovies() {
+    @GetMapping
+    public ResponseEntity<ApiResponse<List<Movie>>> getMovies() {
         try {
-            return new ApiResponse<>("Movies successfully retrieved", movieService.getMovies(), 200);
+            return ResponseEntity
+                    .status(200)
+                    .body(new ApiResponse<>("Movies successfully retrieved", this.movieService.getMovies(), 200));
         } catch (Exception e) {
-            return new ApiResponse<>(e.getMessage(), null, 500);
+            return ResponseEntity
+                    .status(500)
+                    .body(new ApiResponse<>(e.getMessage(), null, 500));
         }
     }
 }
