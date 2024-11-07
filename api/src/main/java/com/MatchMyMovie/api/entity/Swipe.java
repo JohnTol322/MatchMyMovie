7697da14d136ce362d560fd6a0ef8b6ed26b921e
John Tol <johntol322@gmail.com>
2024-11-07 17:15:13 +0100

    #4 - Created Swipe entity and related it to User

---
+++ api/src/main/java/com/MatchMyMovie/api/entity/Swipe.java
@@ -0,0 +1,68 @@
+package com.MatchMyMovie.api.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "swipes")
+public class Swipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private Long movieId;
+
+    private boolean liked;
+
+    private Date createdOn;
+
+    public Swipe() {
+        this.createdOn = new Date();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Long getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(Long movieId) {
+        this.movieId = movieId;
+    }
+
+    public boolean isLiked() {
+        return liked;
+    }
+
+    public void setLiked(boolean liked) {
+        this.liked = liked;
+    }
+
+    public Date getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(Date createdOn) {
+        this.createdOn = createdOn;
+    }
+}
