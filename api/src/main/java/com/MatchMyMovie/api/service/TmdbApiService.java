b66bf6bd3c7fd9a14a31d756f70595c7e9f6a655
John Tol <johntol322@gmail.com>
2024-11-11 21:50:53 +0100

    Removed imports that were not used

--- api/src/main/java/com/MatchMyMovie/api/service/TmdbApiService.java
+++ api/src/main/java/com/MatchMyMovie/api/service/TmdbApiService.java
@@ -1,31 +1,24 @@
 package com.MatchMyMovie.api.service;
 
-import com.MatchMyMovie.api.model.movie.Movie;
 import com.MatchMyMovie.api.model.movie.MovieDetails;
 import com.MatchMyMovie.api.model.movie.TmdbResponse;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
 
 @Service
 public class TmdbApiService {
 
     private final String apiKey;
     private final RestTemplate restTemplate;
-    private final StringHttpMessageConverter stringHttpMessageConverter;
 
-    public TmdbApiService(RestTemplate restTemplate, @Value("${TMDB_API_KEY}") String apiKey, StringHttpMessageConverter stringHttpMessageConverter) {
+    public TmdbApiService(RestTemplate restTemplate, @Value("${TMDB_API_KEY}") String apiKey) {
         this.restTemplate = restTemplate;
         this.apiKey = apiKey;
-        this.stringHttpMessageConverter = stringHttpMessageConverter;
     }
 
     public TmdbResponse discoverMovies() {
