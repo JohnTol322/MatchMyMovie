5028de7d64f137d403e5577344d82a1cd09355db
John Tol <johntol322@gmail.com>
2024-11-19 12:10:56 +0100

    #6 - Improved login page by implementing design

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Register from "./components/Register";
-import Login from "./components/Login";
 import HomePage from "./components/home/HomePage";
 import SwipePage from "./components/matcher/SwipePage";
+import LoginPage from "./components/login/LoginPage";
 
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path={'/register'} element={<Register/>}/>
-                <Route path={'/login'} element={<Login/>}/>
+                <Route path={'/login'} element={<LoginPage/>}/>
                 <Route path={'/matcher'} element={<SwipePage/>}/>
                 <Route path={'/'} element={<HomePage/>}/>
             </Routes>
---
+++ web/src/components/login/LoginPage.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import {LoginDetails} from "../../models/LoginDetails";
+import {authService} from "../../services/AuthService";
+import {Link, useNavigate} from "react-router-dom";
+import "./LoginPage.scss";
+
+const LoginPage = () => {
+    const [loginDetails, setLoginDetails] = React.useState<LoginDetails>({
+        email: "",
+        password: "",
+    });
+    const [isLoading, setIsLoading] = React.useState(false);
+
+    const navigate = useNavigate();
+
+    const handleSubmit = (e: any) => {
+        e.preventDefault();
+        setIsLoading(true);
+        authService.login(loginDetails).then((success) => {
+            if (success) {
+                console.log("Login successful");
+                setTimeout(() => {
+                    setIsLoading(false);
+                    navigate("/matcher");
+                }, 500);
+            } else {
+                console.log("Login failed");
+                setIsLoading(false);
+            }
+        });
+    };
+
+    return (
+        <div className="page-background">
+            <div className="form-container">
+                <div className="login-title">
+                    <b>Login</b>
+                    <p>Don’t have an account? <Link className="register-link" to="/register">Create your free account here.</Link></p>
+                </div>
+                <div className="input-fields">
+                    <input type="email" placeholder="E-mail" onChange={(e) => {
+                        setLoginDetails({...loginDetails, email: e.target.value});
+                    }}/>
+                    <input type="password" placeholder="Password" onChange={(e) => {
+                        setLoginDetails({...loginDetails, password: e.target.value});
+                    }}/>
+                </div>
+                <button style={{cursor: isLoading ? "default" : "cursor"}} disabled={isLoading} onClick={handleSubmit}>
+                    {isLoading ? "Loading..." : "Login"}
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default LoginPage;
